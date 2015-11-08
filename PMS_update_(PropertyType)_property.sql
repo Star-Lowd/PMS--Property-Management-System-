fe296dba1c735fbe7f1495171e3a45060f717d1d
Star-Lowd <stargod09@gmail.com>
2015-11-09 00:28:19 +0700

    Modify Tenant, Add Tenant
    Modify Tenant, Add Tenant (using the right mouse button click event )

---
+++ PMS_update_(PropertyType)_property.sql
@@ -0,0 +1,86 @@
+USE master
+GO
+if exists (select * from sysdatabases where name='PMS')
+		drop database PMS
+go
+create database PMS
+go
+use PMS
+go
+create table Manager
+(
+ManagerID int identity (10,1) primary key,
+Fullname varchar(80) not null,
+Gender varchar(10),
+DOB date,
+PhoneNumber int not null,
+EmailAddress varchar (50),
+[Address] varchar(150),
+Username varchar(20) not null,
+[Password] varchar(15) not null check ([password] >= 8)
+
+)
+create table LandLord
+(
+LandLordID int identity (10,1) primary key,
+Fullname varchar(80) not null,
+Gender varchar(10),
+DOB date,
+PhoneNumber int not null,
+EmailAddress varchar (50),
+[Address] varchar(150),
+Username varchar(20) not null,
+[Password] varchar(15) not null check ([password] >= 8),
+ManagerID int foreign key  references Manager(ManagerID)
+)
+
+create table Property 
+(
+PropertyID int identity (1,1) primary key,
+[Address] varchar(150) not null,
+Price money,
+PropertyType varchar(20) not null,
+Size int,
+Discription ntext,
+[Status] ntext,
+Building varchar(20),
+LivingArea varchar(20),
+NumberOfRooms int,
+NumberOfBathroom int,
+Imagefolder varchar(150),
+ManagerID int foreign key  references Manager(ManagerID),
+LandLordID int foreign key  references LandLord(LandLordID)
+)
+
+create table Tenant 
+(
+TenantID int identity (1,1) primary key,
+Fullname varchar(80) not null,
+Gender varchar(10),
+DOB date,
+PhoneNumber int not null,
+EmailAddress varchar (50),
+MovingDate date,
+LeaseEndDate date,
+[Status] varchar(15),
+PropertyID int  foreign key  references Property (PropertyID)
+)
+
+
+
+Select * from Manager
+
+
+select * from LandLord
+
+
+select * from Tenant
+
+
+select * from Property
+go
+
+insert into Manager(Fullname,Gender,DOB,[Address],PhoneNumber,EmailAddress,Username,[Password]) 
+values ('ManagerName','Male','09-05-1992','ManagerAddress',123456789,'ManagerEmail','ManagerUsername',1234567)
+
+insert into Property (Address,PropertyType) values('Blah blah blah','Estate')
